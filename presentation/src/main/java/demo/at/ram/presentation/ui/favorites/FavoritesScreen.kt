b11e782768a20a2bd89d15274e94f1583b45fcc8
anhtuanm <anhtuan.mai19@gmail.com>
2026-03-30 18:55:41 +0200

    Improve FavoritesScreen UI and update image assets
    - Update `FavoritesScreen` to use `CircularProgressIndicator` for the loading state.
    - Implement an empty state view featuring a "no content" image.
    - Enhance the error state to display dynamic error messages centered on the screen.
    - Replace `builder.png` with `builder.jpg` and add the `no_content.jpg` drawable asset.

--- presentation/src/main/java/demo/at/ram/presentation/ui/favorites/FavoritesScreen.kt
+++ presentation/src/main/java/demo/at/ram/presentation/ui/favorites/FavoritesScreen.kt
@@ -1,12 +1,20 @@
 package demo.at.ram.presentation.ui.favorites
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import demo.at.ram.presentation.ui.home.CharacterCardList
 import demo.at.ram.presentation.ui.log.LogCompositions
+import demo.at.ram.shared.R
 
 @Composable
 fun FavoritesScreen(
@@ -27,14 +35,33 @@ fun FavoritesContent(uiState: FavoritesUiState, unsetFavorite: (Long) -> Unit) {
     LogCompositions("FavoritesContent")
 
     when (uiState) {
-        is FavoritesUiState.Loading -> {Text("loading")}
-        is FavoritesUiState.Empty -> {Text("no favorites")}
+        is FavoritesUiState.Loading -> {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
+        }
+
+        is FavoritesUiState.Empty -> {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Image(
+                    painter = painterResource(id = R.drawable.no_content),
+                    contentDescription = "no favorites"
+                )
+            }
+        }
+
         is FavoritesUiState.Success -> {
             CharacterCardList(
                 characters = uiState.characters,
                 onCharacterClick = unsetFavorite
             )
         }
-        is FavoritesUiState.Error -> {Text("error")}
+
+        is FavoritesUiState.Error -> {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text("${uiState.throwable.message ?: "Unknown error"}")
+            }
+
+        }
     }
 }
